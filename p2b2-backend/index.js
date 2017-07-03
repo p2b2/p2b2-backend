3048a70ee61bd9d11c75c95a269ea2c7bdaa6f2a
Steffen Härtlein <steffen.haertlein@gmx.de>
2017-07-03 19:19:43 +0200

    improved backend code before integration of analyzer

--- p2b2-backend/index.js
+++ p2b2-backend/index.js
@@ -11,22 +11,26 @@ const port = 3000
 
 winston.level = "debug";
 
+client.on("error", err => {
+    winston.error(err)
+})
+
+let validateAddress = (req, res, next) => {
+    let address = req.params.address || ""
+    if(!address.startsWith("0x")){
+        address = "0x" + address
+    }
+    if(web3.isAddress(address)){
+        next()
+    } else {
+        res.status(400).send(req.params.address + " is not a valid Ethereum address!")
+    }
+}
+
 let bootstrap = function () {
     let baseApp = express()
 
-    baseApp.all("/:address/*", (req, res, next) => {
-        let address = req.params.address || ""
-        if(!address.startsWith("0x")){
-            address = "0x" + address
-        }
-        if(web3.isAddress(address)){
-            next()
-        } else {
-            next(new Error(400))
-        }
-    })
-
-    baseApp.get('/:address', (req, res) => {
+    baseApp.get('/:address', validateAddress, (req, res) => {
         let address = req.params.address
         client.get(address, (error, result) => {
             winston.debug(result)
